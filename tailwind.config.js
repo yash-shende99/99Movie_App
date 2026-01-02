4b0d428a8cb5f710994b96e39056795af2345a5d
yash-shende99 <yashshende9999@gmail.com>
2026-01-02 19:02:44 +0530

    feat: initial implementation of tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   presets: [require('nativewind/preset')],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light:{
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB"
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+        accent: "#AB8BFF"
+      },
+    },
+  },
+  plugins: [],
+}
+
